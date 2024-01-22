3301b04a0d1dba76014cbdb3ba9719750d8224d9
Jason Wu <jason@jiliguala.com>
2024-01-22 13:31:15 +0800

    feat: build

---
+++ src/router/index.js
@@ -0,0 +1,226 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import login from "@/views/login";
+import home from "@/views/home";
+import console from "@/views/console";
+import teprunner from "@/views/teprunner";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/login",
+    meta: {
+      title: "测试平台登录",
+    },
+    component: login,
+  },
+  {
+    path: "/",
+    name: "home",
+    meta: {
+      requireAuth: true,
+    },
+    component: home,
+    redirect: "teprunner/grammar",
+    children: [
+      {
+        path: "teprunner",
+        name: "teprunner",
+        meta: {
+          title: "接口自动化",
+        },
+        component: teprunner,
+        redirect: "teprunner/grammar",
+        children: [
+          {
+            path: "grammar",
+            meta: {
+              title: "语法说明",
+            },
+            component: () => import("@/views/teprunner/Grammar.vue"),
+          },
+          {
+            path: "envVar",
+            meta: {
+              title: "环境变量",
+            },
+            component: () => import("@/views/teprunner/EnvVar.vue"),
+          },
+          {
+            path: "fixture",
+            name: "fixture",
+            meta: {
+              title: "fixtures",
+            },
+            component: () => import("@/views/teprunner/Fixture.vue"),
+            children: [
+              {
+                path: "addFixture",
+                name: "addFixture",
+                meta: {
+                  title: "新增 fixture",
+                },
+                component: () => import("@/views/teprunner/FixtureEditor"),
+              },
+              {
+                path: "editFixture",
+                name: "editFixture",
+                meta: {
+                  title: "编辑 fixture",
+                },
+                component: () => import("@/views/teprunner/FixtureEditor"),
+              },
+            ],
+          },
+          {
+            path: "case",
+            name: "case",
+            meta: {
+              title: "用例管理",
+            },
+            component: () => import("@/views/teprunner/case/CaseManagement.vue"),
+            children: [
+              {
+                path: "addCase",
+                name: "addCase",
+                meta: {
+                  title: "新增用例",
+                },
+                component: () => import("@/views/teprunner/case/CaseEditor"),
+              },
+              {
+                path: "editCase",
+                name: "editCase",
+                meta: {
+                  title: "编辑用例",
+                },
+                component: () => import("@/views/teprunner/case/CaseEditor"),
+              },
+              {
+                path: "caseView",
+                name: "caseView",
+                meta: {
+                  title: "查看用例",
+                },
+                component: () => import("@/views/teprunner/case/CaseView"),
+              },
+              {
+                path: "caseResult",
+                name: "case.caseResult",
+                meta: {
+                  title: "用例运行结果",
+                },
+                component: () => import("@/views/teprunner/case/CaseResult"),
+              },
+            ],
+          },
+          {
+            path: "plan",
+            name: "plan",
+            meta: {
+              title: "测试计划",
+            },
+            component: () => import("@/views/teprunner/plan/PlanManagement.vue"),
+            children: [
+              {
+                path: "addPlan",
+                name: "addPlan",
+                meta: {
+                  title: "添加计划",
+                },
+                component: () => import("@/views/teprunner/plan/PlanEditor"),
+              },
+              {
+                path: "editPlan",
+                name: "editPlan",
+                meta: {
+                  title: "编辑计划",
+                },
+                component: () => import("@/views/teprunner/plan/PlanEditor"),
+              },
+              {
+                path: "caseList",
+                name: "caseList",
+                meta: {
+                  title: "用例列表",
+                },
+                component: () => import("@/views/teprunner/plan/CaseList"),
+              },
+              {
+                path: "planResult",
+                name: "planResult",
+                meta: {
+                  title: "计划运行结果",
+                },
+                component: () => import("@/views/teprunner/plan/PlanResult"),
+                children: [
+                  {
+                    path: "caseResult",
+                    name: "plan.caseResult",
+                    meta: {
+                      title: "用例运行结果",
+                    },
+                    component: () => import("@/views/teprunner/plan/CaseResult"),
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            path: "gitSync",
+            meta: {
+              title: "Git同步",
+            },
+            component: () => import("@/views/teprunner/GitSync.vue"),
+          },
+        ],
+      },
+      {
+        path: "console",
+        name: "console",
+        meta: {
+          title: "后台管理",
+        },
+        component: console,
+        redirect: "console/userManagement",
+        children: [
+          {
+            path: "userManagement",
+            meta: {
+              title: "用户管理",
+            },
+            component: () => import("@/views/console/userManagement.vue"),
+          },
+          {
+            path: "projectManagement",
+            meta: {
+              title: "项目管理",
+            },
+            component: () => import("@/views/console/ProjectManagement.vue"),
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(auth => auth.meta.requireAuth)) {
+    let token = localStorage.getItem("token");
+    if (token) {
+      next();
+    } else {
+      next({
+        path: "/login",
+      });
+    }
+  } else {
+    next();
+  }
+});
+export default router;
---
+++ src/views/teprunner/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <NavLeft>
+    <div slot="menuItem">
+      <el-menu-item index="/teprunner/grammar">
+        <i class="el-icon-s-opportunity"></i>
+        <span slot="title">语法说明</span>
+      </el-menu-item>
+      <el-menu-item index="/teprunner/envVar">
+        <i class="el-icon-coin"></i>
+        <span slot="title">环境变量</span>
+      </el-menu-item>
+      <el-menu-item index="/teprunner/fixture">
+        <i class="el-icon-magic-stick"></i>
+        <span slot="title">fixtures</span>
+      </el-menu-item>
+      <el-menu-item index="/teprunner/case">
+        <i class="el-icon-edit-outline"></i>
+        <span slot="title">用例管理</span>
+      </el-menu-item>
+      <el-menu-item index="/teprunner/plan">
+        <i class="el-icon-alarm-clock"></i>
+        <span slot="title">测试计划</span>
+      </el-menu-item>
+      <el-menu-item index="/teprunner/gitSync">
+        <i class="el-icon-attract"></i>
+        <span slot="title">Git同步</span>
+      </el-menu-item>
+    </div>
+  </NavLeft>
+</template>
+<script>
+import NavLeft from "@/components/NavLeft";
+
+export default {
+  components: {
+    NavLeft,
+  },
+  mounted() {
+    this.saveProjectEnv();
+  },
+  methods: {
+    saveProjectEnv() {
+      let localProjectEnvList = JSON.parse(localStorage.getItem("projectEnvList"));
+      if (!localProjectEnvList) {
+        this.$http
+          .get("/teprunner/projects/env")
+          .then(({ data: { projectEnvList, curProjectEnv } }) => {
+            if (projectEnvList) {
+              this.projectEnvList = projectEnvList;
+              localStorage.setItem("projectEnvList", JSON.stringify(projectEnvList));
+              let localCurProjectEnv = JSON.parse(localStorage.getItem("curProjectEnv"));
+              if (!localCurProjectEnv) {
+                this.curProjectId = curProjectEnv.curProjectId;
+                this.curProjectName = curProjectEnv.curProjectName;
+                this.curEnvName = curProjectEnv.curEnvName;
+                localStorage.setItem("curProjectEnv", JSON.stringify(curProjectEnv));
+              }
+            }
+          })
+          .finally(() => {});
+      }
+    },
+  },
+};
+</script>
